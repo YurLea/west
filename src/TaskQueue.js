d3db7ad076c6314bedeab903060fa2e5edbd0ae2
YurLea <vsailevyrui@gmail.com>
2026-03-17 15:59:08 +0300

    1 done

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,10 +1,10 @@
-const TaskQueue = function() {
+/*const TaskQueue = function() {
     function TaskQueue() {
         this.tasks = [];
         this.running = false;
     }
 
-    TaskQueue.prototype.push = function(run, dispose, duration) {
+    /*TaskQueue.prototype.push = function(run, dispose, duration) {
         if (duration === undefined || duration === null) {
             this.tasks.push({runAndContinue: run, dispose});
         } else {
@@ -19,13 +19,13 @@ const TaskQueue = function() {
             });
         }
         runNextTask(this);
-    };
+    };*/
 
-    TaskQueue.prototype.continueWith = function(action) {
+    /*TaskQueue.prototype.continueWith = function(action) {
         this.push(action, null, 0);
-    };
+    };*/
 
-    function runNextTask(taskQueue) {
+    /*function runNextTask(taskQueue) {
         if (taskQueue.running || taskQueue.tasks.length === 0) {
             return;
         }
@@ -47,9 +47,61 @@ const TaskQueue = function() {
         else {
             runNextTask(taskQueue);
         }
+    }*/
+
+    /*return TaskQueue;
+}();*/
+
+function runNextTask(taskQueue) {
+    if (taskQueue.running || taskQueue.tasks.length === 0) {
+        return;
+    }
+    taskQueue.running = true;
+    const task = taskQueue.tasks.shift();
+
+    if (task.runAndContinue) {
+        setTimeout(() => {
+            task.runAndContinue(() => {
+                task.dispose && task.dispose();
+                taskQueue.running = false;
+
+                setTimeout(() => {
+                    runNextTask(taskQueue);
+                });
+            });
+        }, 0);
+    }
+    else {
+        runNextTask(taskQueue);
+    }
+}
+
+export default class TaskQueue {
+    constructor (){
+        this.tasks = [];
+        this.running = false;
     }
 
-    return TaskQueue;
-}();
+    push (run, dispose, duration) {
+        if (duration === undefined || duration === null) {
+            this.tasks.push({runAndContinue: run, dispose});
+        } else {
+            this.tasks.push({
+                runAndContinue: (continuation) => {
+                    run();
+                    setTimeout(() => {
+                        continuation();
+                    }, duration);
+                },
+                dispose
+            });
+        }
+        runNextTask(this);
+    }
 
-export default TaskQueue;
+    continueWith (action) {
+        this.push(action, null, 0);
+    }
+}
+
+
